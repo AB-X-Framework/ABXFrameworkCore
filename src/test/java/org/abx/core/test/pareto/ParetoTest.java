b21343f66a253b72f6fa5df6863071313db516b5
luis <llara@appvance.com>
2024-11-15 15:00:53 -0600

    Fix typo

--- src/test/java/org/abx/core/test/pareto/ParetoTest.java
+++ src/test/java/org/abx/core/test/pareto/ParetoTest.java
@@ -4,13 +4,19 @@ import org.abx.core.ABMFrameworkCore;
 import org.abx.core.syntax.OptimizationLevel;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class ParetoTest {
 
     @Test
     public void test() throws Exception {
-        ABMFrameworkCore abmFrameworkCore = new ABMFrameworkCore(OptimizationLevel.performance);
-        abmFrameworkCore.processFile(
-                "src/test/java/org/abx/core/test/pareto/AllExecs.js");
+        try {
+            ABMFrameworkCore abmFrameworkCore = new ABMFrameworkCore(OptimizationLevel.performance);
+            abmFrameworkCore.processFile(
+                    "src/test/java/org/abx/core/test/pareto/AllExecs.js");
+        }finally {
+            new File("src/test/java/org/abx/core/test/pareto/pareto.ab-x.csv").delete();
+        }
 
     }
 }
